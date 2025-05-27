288c0824efc08ccced73b0ca81daa37e2aa8aa67
francobeneforti <francobeneforti@hotmail.com>
2025-05-27 18:40:38 -0300

    Fix de test_e2e.test_user

--- app/test/test_e2e/test_user.py
+++ app/test/test_e2e/test_user.py
@@ -36,8 +36,8 @@ class RegistrationE2ETest(BaseE2ETest):
         # Enviar el formulario
         self.page.click("button[type='submit']")
 
-        # Verificar que se redirige a events
-        self.page.wait_for_url(f"{self.live_server_url}/events/*")
+        # Verificar que se redirige a la home
+        expect(self.page).to_have_url(self.live_server_url + "/")
 
         # Verificar que el usuario fue creado en la base de datos
         self.assertTrue(User.objects.filter(username="nuevo_usuario").exists())
@@ -56,8 +56,8 @@ class RegistrationE2ETest(BaseE2ETest):
         # Enviar el formulario
         self.page.click("button[type='submit']")
 
-        # Verificar redirección y verificación en la base de datos
-        self.page.wait_for_url(f"{self.live_server_url}/events/*")
+        # Verificar redirección a la home y verificación en la base de datos
+        expect(self.page).to_have_url(self.live_server_url + "/")
         self.assertTrue(User.objects.filter(username="nuevo_usuario").exists())
 
     def test_duplicate_email_registration(self):
@@ -138,8 +138,8 @@ class LoginE2ETest(BaseE2ETest):
         # Utilizar el método auxiliar para iniciar sesión
         self.login_user(user.username, "password123")
 
-        # Verificar que se redirige a events
-        self.page.wait_for_url(f"{self.live_server_url}/events/*")
+        # Verificar que se redirige a la home
+        expect(self.page).to_have_url(self.live_server_url + "/")
 
     def test_invalid_credentials_login(self):
         """Verifica el intento de login con credenciales inválidas"""
